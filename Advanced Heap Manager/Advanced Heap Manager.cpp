0c0979713ccf63e726f022ecb95593002012e020
Kare2311 <bojankarovic32@gmail.com>
2024-11-19 21:39:13 +0100

    Add project files.

---
+++ Advanced Heap Manager/Advanced Heap Manager.cpp
@@ -0,0 +1,17 @@
+// Advanced Heap Manager.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    
+}
+
+
