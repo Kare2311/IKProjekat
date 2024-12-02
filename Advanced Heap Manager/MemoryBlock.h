a4bff0f926c0f0e2f292304af9b47eb3ce97b9ce
Kare2311 <bojankarovic32@gmail.com>
2024-12-02 23:57:35 +0100

    proba

---
+++ Advanced Heap Manager/MemoryBlock.h
@@ -0,0 +1,13 @@
+﻿#ifndef MEMORY_BLOCK_H
+#define MEMORY_BLOCK_H
+
+#include <cstddef> // za size_t
+
+struct MemoryBlock {
+    void* address;    // Adresa bloka memorije
+    size_t size;      // Veličina bloka memorije
+    bool free;        // Status: true ako je slobodan, false ako je zauzet
+    int userID;       // ID korisnika koji koristi ovaj blok
+};
+
+#endif // MEMORY_BLOCK_H
